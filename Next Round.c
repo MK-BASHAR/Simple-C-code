dab1db18fe89c2390d60ad7b54d9da10b2cdd011
M.K BASHAR <mkbashar6161@gmail.com>
2026-09-18 11:34:00 +0600

    Add files via upload
    this is my codeforces problem solution

---
+++ Next Round.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    int a[50];
+    int count = 0;
+
+    scanf("%d %d", &n, &k);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] >= a[k - 1] && a[i] > 0) {
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+
+    return 0;
+}
